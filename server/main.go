8b33367a2396d8f65b6ec44ef51260b9b740bf4d
Sam Poate <sampoate@outlook.com>
2020-04-19 18:54:25 +0100

    allow cors

--- server/main.go
+++ server/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"./router"
 
-	"github.com/gorilla/handlers"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := router.Router()
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://127.0.0.1:5500"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(r)
+
 	fmt.Println("Starting server on the port 8080...")
 	fmt.Println("===================================")
 
-	http.ListenAndServe(":8080", handlers.CORS()(r))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
